115ebf95c4c1f2989c61202d1d4a8257a873cfce
milton-reyes <altf4616@outlook.com>
2020-12-23 13:47:52 -0500

    main method

---
+++ src/main/java/com/yukaju/ArticleSrvApplication.java
@@ -0,0 +1,11 @@
+package com.yukaju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleSrvApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleSrvApplication.class, args);
+	}
+}
